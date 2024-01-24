3b44cad6c0d06c6b7b606e5fb37667f3de941867
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2024-01-24 11:27:54 +0100

    (feat) Validate form via the Contraint Validation API  and display error messages

--- assets/js/assurance.js
+++ assets/js/assurance.js
@@ -1,24 +1,49 @@
-function validateForm(age, anciennete, permis, accident) {
-	switch (true) {
-		case isNaN(age):
-			return "Merci de renseigner votre âge";
-		case isNaN(anciennete):
-			return "Merci de renseigner votre ancienneté";
-		case isNaN(permis):
-			return "Merci de renseigner depuis combien d'année vous possédez le permis";
-		case isNaN(accident):
-			return "Merci de renseigner combien d'accidents vous avez eu";
-		case accident < 0 || age < 0 || anciennete < 0 || permis < 0:
-			return "Merci de ne renseigner que des valeurs positives.";
-		default:
-			return "";
+// Form validation ==================================
+
+function checkValidation(event) {
+	let errorMessage = document.querySelector(`#${event.target.id}+p`);
+
+	if (event.target.validity.rangeUnderflow) {
+		// Negative values
+		event.target.classList.toggle(":out-of-range");
+		errorMessage.innerText = "Veuillez entrer une valeur positive.";
+		errorMessage.style.display = "inline-block";
+		return false;
+		// event.target.setCustomValidity("Veuillez entrer une valeur positive.");
+	} else if (event.target.validity.valueMissing) {
+		// Empty
+		event.target.classList.toggle(":invalid");
+		errorMessage.innerText = "Veuillez renseigner l'information";
+		errorMessage.style.display = "inline-block";
+		return false;
+		// event.target.setCustomValidity("Veuillez renseigner l'information");
+	} else {
+		event.target.classList.toggle(":valid");
+		errorMessage.style.display = "none";
+		return true;
+	}
+}
+
+function checkAllValid(validationStatus) {
+	for (let id in validationStatus) {
+		if (validationStatus.id == false) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// Simulation tarif ==================================
+
+function clearOuput(sectionResult) {
+	let child = sectionResult.firstChild;
+	if (child != null) {
+		sectionResult.removeChild(child);
 	}
 }
 
 function calculateTarif(age, anciennete, permis, accident, sectionResult) {
 	const tarifs = ["rouge", "orange", "vert", "bleu"];
-
-	let result = document.createElement("p");
 
 	let echelon = 0; // Default tarif
 
@@ -34,6 +59,7 @@ function calculateTarif(age, anciennete, permis, accident, sectionResult) {
 		echelon++;
 	}
 
+	let result = document.createElement("p");
 	if (echelon < 0) {
 		result.innerText = "Vous ne pouvez pas bénéficier de notre assurance.";
 	} else {
@@ -43,24 +69,53 @@ function calculateTarif(age, anciennete, permis, accident, sectionResult) {
 	sectionResult.replaceChildren(result);
 }
 
-let submit = document.querySelector("#submit");
+// Main ==================================
+
+// Check that inputs are valid
+const form = document.querySelector("form");
+let validationStatus = {
+	age: false,
+	anciennete: false,
+	permis: false,
+	accident: false,
+};
+form.addEventListener("focusout", (event) => {
+	let id = event.target.id;
+	if (checkValidation(event)) {
+		validationStatus.id = true;
+	} else {
+		validationStatus.id = false;
+	}
+});
+
+// If all fields are valid, calculate tarif
+const submit = document.querySelector("#submit");
+const inputs = document.querySelector("input");
 submit.addEventListener("click", (event) => {
 	event.preventDefault();
 
-	// Fetch input
-	let age = parseInt(document.querySelector("#age").value);
-	let anciennete = parseInt(document.querySelector("#anciennete").value);
-	let permis = parseInt(document.querySelector("#permis").value);
-	let accident = parseInt(document.querySelector("#accident").value);
-	let sectionResult = document.querySelector("#result");
-	let errorMessage = document.createElement("p");
+	const sectionResult = document.querySelector("#result");
 
-	errorMessage.innerText = validateForm(age, anciennete, permis, accident);
+	if (checkAllValid(validationStatus)) {
+		// All input fields are valid
+		// Fetch input
+		const age = parseInt(document.querySelector("#age").value);
+		const anciennete = parseInt(
+			document.querySelector("#anciennete").value
+		);
+		const permis = parseInt(document.querySelector("#permis").value);
+		const accident = parseInt(document.querySelector("#accident").value);
 
-	if (errorMessage.innerText == "") {
 		calculateTarif(age, anciennete, permis, accident, sectionResult);
+
+		// Reset
+		validationStatus = {
+			age: false,
+			anciennete: false,
+			permis: false,
+			accident: false,
+		};
 	} else {
-		errorMessage.classList.toggle("error");
-		sectionResult.replaceChildren(errorMessage);
+		clearOuput(sectionResult);
 	}
 });
